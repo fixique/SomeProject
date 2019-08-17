039ebc7fca181a7e5447ca6aedc7fe74647d807a
Krupenko Validislav <dev.fixique@gmail.com>
2019-08-17 18:27:33 +0300

    fix layout for song cell

--- SomeProject/Flows/PlayLIst/PlayList/View/Adapter/Cells/SongCell/SongCell.swift
+++ SomeProject/Flows/PlayLIst/PlayList/View/Adapter/Cells/SongCell/SongCell.swift
@@ -19,6 +19,8 @@ final class SongCell: UITableViewCell {
 
         static let separatorHeight: CGFloat = 1.0
         static let animationDuration: TimeInterval = 0.3
+
+        static let authorLabelTopOffset: CGFloat = 10.0
     }
 
     // MARK: – Private Properties
@@ -52,6 +54,8 @@ final class SongCell: UITableViewCell {
 
     func configure(with model: SongViewModel) {
         songImageView.downloadImage(urlPath: model.songImagePath, placeholder: UIImage(asset: Asset.ImagePlaceholders.imagePlaceholder))
+        songNameLabel.text = model.songName
+        songAuthorLabel.text = model.songAuthor
     }
 
 }
@@ -64,6 +68,8 @@ private extension SongCell {
         selectionStyle = .none
         configureImageView()
         configureSeparator()
+        configureSongNameLabel()
+        configureSongAuthorLabel()
     }
 
     func configureImageView() {
@@ -93,6 +99,37 @@ private extension SongCell {
         let heightConstraint = separatorView.heightAnchor.constraint(equalToConstant: Constants.separatorHeight)
 
         NSLayoutConstraint.activate([leftConstraint, rightConstraint, bottomConstraint, heightConstraint])
+    }
+
+    func configureSongNameLabel() {
+        songNameLabel.textColor = Color.Main.text
+        songNameLabel.font = .systemFont(ofSize: 18.0, weight: .medium)
+        songNameLabel.numberOfLines = 0
+        songNameLabel.text = nil
+        songNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(songNameLabel)
+
+        let topConstraint = songNameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: Constants.commonOffset)
+        let leftConstraint = songNameLabel.leftAnchor.constraint(equalTo: songImageView.rightAnchor, constant: Constants.commonOffset)
+        let rightConstraint = songNameLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -Constants.commonOffset)
+
+        NSLayoutConstraint.activate([topConstraint, leftConstraint, rightConstraint])
+    }
+
+    func configureSongAuthorLabel() {
+        songAuthorLabel.textColor = Color.Main.text
+        songAuthorLabel.font = .systemFont(ofSize: 14.0, weight: .light)
+        songAuthorLabel.numberOfLines = 0
+        songAuthorLabel.text = nil
+        songAuthorLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(songAuthorLabel)
+
+        let topConstraint = songAuthorLabel.topAnchor.constraint(equalTo: songNameLabel.bottomAnchor, constant: Constants.authorLabelTopOffset)
+        let leftConstraint = songAuthorLabel.leftAnchor.constraint(equalTo: songImageView.rightAnchor, constant: Constants.commonOffset)
+        let rightConstraint = songAuthorLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -Constants.commonOffset)
+        let bottomConstraint = songAuthorLabel.bottomAnchor.constraint(lessThanOrEqualTo: contentView.bottomAnchor, constant: -Constants.commonOffset)
+
+        NSLayoutConstraint.activate([topConstraint, leftConstraint, rightConstraint, bottomConstraint])
     }
 
 }
